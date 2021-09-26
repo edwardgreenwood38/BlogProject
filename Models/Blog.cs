89cc2d4357a0b87c11dcef711335727478696a1a
Edward E Greenwood <edwardgreenwood38@gmail.com>
2021-09-25 20:27:54 -0400

    minor change to razor code
    changed item reference to blog to be more descriptive

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -38,6 +38,7 @@ namespace BlogProject.Models
 
 
         // navigation property
+        [Display(Name = "Author")]
         public virtual BlogUser BlogUser { get; set; }
         public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
     }
