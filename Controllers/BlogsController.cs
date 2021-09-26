aca5006430939825fc89cd9fcfeaef6ef2e3b203
Edward E Greenwood <edwardgreenwood38@gmail.com>
2021-09-25 21:32:25 -0400

    changes to blog create
    changes to controller and to create view

--- Controllers/BlogsController.cs
+++ Controllers/BlogsController.cs
@@ -48,7 +48,6 @@ namespace BlogProject.Controllers
         // GET: Blogs/Create
         public IActionResult Create()
         {
-            ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id");
             return View();
         }
 
@@ -91,7 +90,7 @@ namespace BlogProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogUserId,Name,Description,Created,Updated,ImageData,ContentType")] Blog blog)
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Description,Image")] Blog blog)
         {
             if (id != blog.Id)
             {
